c5a072171ce0310f3bb149614cdfc0dd5ef44b25
Muhammad Hassan <mhassan4392@gmail.com>
2022-04-13 04:38:45 +0500

    make it better

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import path from "path";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "tailwind.config.js": path.resolve(__dirname, "tailwind.config.js"),
+    },
+  },
+  optimizeDeps: {
+    include: ["tailwind.config.js"],
+  },
+});
