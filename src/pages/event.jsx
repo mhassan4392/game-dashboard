4527e6885fd2b42def94af1124b485703abdc4f0
Muhammad Hassan <mhassan4392@gmail.com>
2022-04-24 10:56:27 +0500

    make it better

--- src/pages/event.jsx
+++ src/pages/event.jsx
@@ -1,3 +1,5 @@
+import { useState, useEffect } from "react";
+import Spinner from "@/components/Spinner/Spinner";
 import {
   TabButton,
   TabItem,
@@ -9,39 +11,68 @@ import EventWidget from "@/components/pages/events/EventWidget";
 import EventHeader from "@/components/pages/events/EventHeader";
 
 const Event = () => {
-  return (
-    <div className="flex flex-col overflow-hidden">
-      {/* Header */}
-      <EventHeader />
+  // loading
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    setLoading(true);
+    const timeout = setTimeout(() => {
+      setLoading(false);
+    }, 1000);
 
-      <Tabs className="mt-1 flex-grow flex flex-col overflow-hidden">
-        <TabsButtons className="flex items-center bg-dark-light mb-1">
-          <TabButton
-            activeClass="tab-active"
-            className="px-3 py-3 text-sm flex flex-col items-center"
-            tab="match"
-          >
-            Match (2)
-          </TabButton>
-          <TabButton
-            activeClass="tab-active"
-            className="px-3 py-3 text-sm flex flex-col items-center"
-            tab="firsthalf"
-          >
-            First Half (2)
-          </TabButton>
-        </TabsButtons>
-        <TabsItems className="overflow-auto scrollbar grow">
-          <TabItem
-            defaultTab
-            tab="match"
-            className="bg-[#2d799b] bg-opacity-25 md:bg-dark"
-          >
-            {[...Array(20)].map((ar, i) => (
-              <EventWidget />
-            ))}
-          </TabItem>
-          <TabItem tab="firsthalf" className="bg-dark"></TabItem>
+    () => clearTimeout(timeout);
+  }, []);
+  return (
+    <div className="flex h-full grow flex-col overflow-hidden">
+      {/* Header */}
+      {!loading && <EventHeader />}
+
+      <Tabs className="mt-1 grow h-full flex flex-col overflow-hidden">
+        {!loading && (
+          <TabsButtons className="flex items-center bg-dark-light mb-1">
+            <TabButton
+              activeClass="tab-active"
+              className="px-3 py-3 text-sm flex flex-col items-center"
+              tab="match"
+            >
+              Match (2)
+            </TabButton>
+            <TabButton
+              activeClass="tab-active"
+              className="px-3 py-3 text-sm flex flex-col items-center"
+              tab="firsthalf"
+            >
+              First Half (2)
+            </TabButton>
+          </TabsButtons>
+        )}
+        <TabsItems className="flex-grow h-full scrollbar overflow-y-auto overflow-x-hidden">
+          {loading && (
+            <div className="h-full flex items-center justify-center grow">
+              <Spinner />
+            </div>
+          )}
+          {!loading && (
+            <>
+              <TabItem
+                defaultTab
+                tab="match"
+                className="bg-[#2d799b] bg-opacity-25 md:bg-dark"
+              >
+                {[...Array(20)].map((ar, i) => (
+                  <div key={i}>
+                    <EventWidget />
+                  </div>
+                ))}
+              </TabItem>
+              <TabItem tab="firsthalf" className="bg-dark">
+                {[...Array(20)].map((ar, i) => (
+                  <div key={i}>
+                    <EventWidget />
+                  </div>
+                ))}
+              </TabItem>
+            </>
+          )}
         </TabsItems>
       </Tabs>
     </div>
--- src/pages/game_result.jsx
+++ src/pages/game_result.jsx
@@ -1,3 +1,5 @@
+import { useState, useEffect } from "react";
+import Spinner from "@/components/Spinner/Spinner";
 import {
   TabButton,
   TabItem,
@@ -9,38 +11,68 @@ import GameResultWidget from "@/components/pages/gameresults/GameResultWidget";
 import GameResultHeader from "@/components/pages/gameresults/GameResultHeader";
 
 const GameResult = () => {
+  // loading
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    setLoading(true);
+    const timeout = setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+
+    () => clearTimeout(timeout);
+  }, []);
   return (
-    <div className="flex-grow flex flex-col overflow-hidden">
-      <GameResultHeader />
+    <div className="flex-grow h-full flex flex-col overflow-hidden">
+      {/* Header */}
+      {!loading && <GameResultHeader />}
 
       <Tabs className="mt-1 flex-grow flex flex-col overflow-hidden">
-        <TabsButtons className="flex items-center bg-dark-light mb-1">
-          <TabButton
-            activeClass="tab-active"
-            className="px-3 py-3 text-sm flex flex-col items-center"
-            tab="match"
-          >
-            Match (2)
-          </TabButton>
-          <TabButton
-            activeClass="tab-active"
-            className="px-3 py-3 text-sm flex flex-col items-center"
-            tab="firsthalf"
-          >
-            First Half (2)
-          </TabButton>
-        </TabsButtons>
+        {!loading && (
+          <TabsButtons className="flex items-center bg-dark-light mb-1">
+            <TabButton
+              activeClass="tab-active"
+              className="px-3 py-3 text-sm flex flex-col items-center"
+              tab="match"
+            >
+              Match (2)
+            </TabButton>
+            <TabButton
+              activeClass="tab-active"
+              className="px-3 py-3 text-sm flex flex-col items-center"
+              tab="firsthalf"
+            >
+              First Half (2)
+            </TabButton>
+          </TabsButtons>
+        )}
         <TabsItems className="overflow-auto scrollbar flex-grow">
-          <TabItem
-            defaultTab
-            tab="match"
-            className="bg-[#2d799b] bg-opacity-25 md:bg-dark"
-          >
-            {[...Array(20)].map((ar, i) => (
-              <GameResultWidget key={i} />
-            ))}
-          </TabItem>
-          <TabItem tab="firsthalf" className="bg-dark"></TabItem>
+          {loading && (
+            <div className="h-full flex items-center justify-center grow">
+              <Spinner />
+            </div>
+          )}
+          {!loading && (
+            <>
+              <TabItem
+                defaultTab
+                tab="match"
+                className="bg-[#2d799b] bg-opacity-25 md:bg-dark"
+              >
+                {[...Array(20)].map((ar, i) => (
+                  <div key={i}>
+                    <GameResultWidget />
+                  </div>
+                ))}
+              </TabItem>
+              <TabItem tab="firsthalf" className="bg-dark">
+                {[...Array(20)].map((ar, i) => (
+                  <div key={i}>
+                    <GameResultWidget />
+                  </div>
+                ))}
+              </TabItem>
+            </>
+          )}
         </TabsItems>
       </Tabs>
     </div>
