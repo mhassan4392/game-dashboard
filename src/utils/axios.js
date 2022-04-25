5fdb1459dec12dfe0b06cd63f0acaec1360dc93c
Muhammad Hassan <mhassan4392@gmail.com>
2022-04-25 11:06:08 +0500

    make it better

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const Axios = axios.create({
   baseURL: "http://devplayerui.cotu.xyz:9901",
-  headers: { "Access-Control-Allow-Origin": "false" },
+  headers: { "Access-Control-Allow-Origin": "*" },
 });
 export default Axios;
