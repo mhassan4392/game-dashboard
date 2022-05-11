efa4de9dc9620329ec609fd32c5d64552fabbeb6
Muhammad Hassan <mhassan4392@gmail.com>
2022-05-11 17:32:03 +0500

    make it better

--- src/components/bet/BetFormModal.jsx
+++ src/components/bet/BetFormModal.jsx
@@ -7,7 +7,7 @@ const BetFormModel = ({ open, onClose }) => {
       className="!bg-dark text-light !rounded-2xl"
       overlay={false}
       onOutsideClick={onClose}
-      containerClass="bg-black bg-opacity-50"
+      containerClass="bg-black bg-opacity-75"
     >
       <Modal.Body className="bg-dark p-0">
         <BetForm modal={true} onClose={onClose} />
